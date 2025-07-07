877c2cc7d80e1ab24b4492b5c33fc1ed1404b03c
Mandalshravan <mandalshravan2004@gmail.com>
2025-07-07 12:38:16 +0530

    final fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
+  },
   preview: {
-    host: true,
     port: 4173,
-    allowedHosts: ['zerodha-frontend-9dz2.onrender.com']
-  }
+  },
 });
