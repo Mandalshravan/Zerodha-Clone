802476047d9a0a803c59d705de56c0b940d67c0f
Mandalshravan <mandalshravan2004@gmail.com>
2025-07-07 11:55:25 +0530

    initial fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import Dashboard from "./landing_page/dashboard/Dashboard.jsx";
 import HomePage from "./landing_page/home/HomePage.jsx";
 import SignupForm from "./landing_page/signup/SignupForm.jsx";
 import LoginForm from "./landing_page/signup/LoginForm.jsx";
@@ -7,10 +6,12 @@ import About from "./landing_page/about/AboutPage.jsx";
 import Product from "./landing_page/products/ProductPage.jsx";
 import Pricing from "./landing_page/pricing/PricingPage.jsx";
 import Support from "./landing_page/support/SupportPage.jsx";
-import { Routes, Route } from "react-router-dom";
 import Navbar from "./landing_page/Navbar.jsx";
 import Fotter from "./landing_page/Fotter.jsx";
 import PageNotFound from "./landing_page/PageNotFound.jsx";
+import Dashboard from "./landing_page/dashboard/Dashboard.jsx"; // ✅ NEW
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} /> {/* ✅ NEW */}
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
