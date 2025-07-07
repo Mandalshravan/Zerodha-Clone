14803450c1dfa6e28f39df4605534fc6ceb77a98
Mandalshravan <mandalshravan2004@gmail.com>
2025-07-07 17:32:53 +0530

    iniatl commit

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -2,6 +2,24 @@ const User = require("../models/UserModel");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+module.exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful" });
+  } catch (err) {
+    console.log("Signup error:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 module.exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -30,7 +48,12 @@ module.exports.login = async (req, res) => {
   }
 };
 
-module.exports.userverification = async (req, res) => {
+module.exports.logout = (req, res) => {
+  // If you're using tokens, logout is usually handled on frontend by deleting token
+  res.json({ message: "Logged out" });
+};
+
+module.exports.verify = async (req, res) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.json({ status: false });
 
